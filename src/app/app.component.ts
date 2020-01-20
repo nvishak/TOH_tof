0a0277f81b83a313dd42f6e7cd0a188bc0aeba00
Kaustub Mayuram <k.mayuram@avendata.com>
2020-01-20 17:29:56 +0530

    TT-13V2 Changes done for the time, and navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tohTof';
+  timer: any;
+  currentTime:any;
+  time:any;
+  ngOnInit(){
+    this.timer = setInterval(() => {
+      this.time = new Date();
+      this.currentTime = this.currentTime = this.time.getDate() + "/"
+      + (this.time.getMonth()+1)  + "/" 
+      + this.time.getFullYear() + " @ "  
+      + this.time.getHours() + ":"  
+      + this.time.getMinutes() + ":" 
+      + this.time.getSeconds();
+    }, 1000);
+  }
 }
