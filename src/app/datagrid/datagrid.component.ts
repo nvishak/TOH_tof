06016749a0269ddbc5bbfec746d866c9c4a2aac3
Kaustub Mayuram <k.mayuram@avendata.com>
2020-01-20 19:29:27 +0530

    Changes done for the filter options hide show functionality

--- src/app/datagrid/datagrid.component.ts
+++ src/app/datagrid/datagrid.component.ts
@@ -26,6 +26,7 @@ export class DatagridComponent implements OnInit {
     value: ''
   };
   disableExport: boolean = false;
+  filterActive: boolean = false;
   ngOnInit() {
     this.timer = setInterval(() => {
       this.time = new Date();
@@ -63,5 +64,18 @@ export class DatagridComponent implements OnInit {
     const wb: xlsx.WorkBook = xlsx.utils.book_new();
     xlsx.utils.book_append_sheet(wb, ws, 'Sheet1');
     xlsx.writeFile(wb, 'exportCSV.xlsx');
+  }
+  openFilters(){
+    if(!this.filterActive){
+    this.filterActive = true;
+    document.getElementById("rightNavBar").style.width = "25%";
+    document.getElementById("rightNavBar").style.right = "0";
+    document.getElementById("setting").style.right = "25%";
+    }else{
+      this.filterActive = false;
+      document.getElementById("rightNavBar").style.width = "0";
+      document.getElementById("rightNavBar").style.right = "-30px";
+      document.getElementById("setting").style.right = "1%";
+    }
   }
 }
